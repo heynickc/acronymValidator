bc9c9be946ff37cceafd6c3974e354d65fef6730
unknown <nchamberlain@BSBZGV1.beltman.local>
2014-08-21 13:04:03 -0400

    Refactoring and testing

--- main.go
+++ main.go
@@ -1,52 +1,17 @@
 package main
 
 import (
-	// "./validator"
+	"./validator"
 	"fmt"
-	"sort"
 	"strings"
 )
 
 func main() {
 
 	productName := strings.Fields(strings.ToLower("GISi Zombie Tracker 3000"))
-	validAcronym := strings.Split(strings.ToLower("GIZTK3"), "")
+	validAcronym := "GIZTK3"
 	// invalidAcronym := "GZT3k"
 
-	acronymLettersContained := make([]bool, len(validAcronym))
-	productsHavingLetter := make([]bool, len(productName))
-	hitsOrder := make([][]int, len(productName))
-	for i := range hitsOrder {
-		hitsOrder[i] = make([]int, 0)
-	}
-	isProductHitInOrder := make([]bool, len(productName))
-
-	fmt.Println(acronymLettersContained)
-	fmt.Println(productsHavingLetter)
-	fmt.Println(isProductHitInOrder)
-
-	for wordIndex, productNameWord := range productName {
-		// Checking word GISi, Zombie, Tracker, 3000
-		for letterIndex, acronymLetter := range validAcronym {
-			// Checking letters G, I, Z, T, K, 3 in word GISi
-			if strings.Contains(productNameWord, acronymLetter) { // Check if the word contains the acronym letter
-				if !acronymLettersContained[letterIndex] { // Check if the acronym letter has been found
-					fmt.Printf("acronym[%v] : %v is contained in productNameWord[%v][%v] : [%v][%v]\n", letterIndex, acronymLetter, wordIndex, strings.Index(productNameWord, acronymLetter), productNameWord, string(productNameWord[strings.Index(productNameWord, acronymLetter)]))
-					acronymLettersContained[letterIndex] = true
-					productsHavingLetter[wordIndex] = true
-					hitsOrder[wordIndex] = append(hitsOrder[wordIndex], strings.Index(productNameWord, acronymLetter))
-				}
-			}
-		}
-	}
-
-	for hitsIndex, hits := range hitsOrder {
-		if sort.IntsAreSorted(hits) {
-			isProductHitInOrder[hitsIndex] = true
-		}
-	}
-
-	fmt.Println(acronymLettersContained)
-	fmt.Println(productsHavingLetter)
-	fmt.Println(isProductHitInOrder)
+	fmt.Println(validator.IsValid(validAcronym, productName))
+	// fmt.Println(validator.IsValid(invalidAcronym, productName))
 }
--- validator/validator.go
+++ validator/validator.go
@@ -1,51 +1,95 @@
 package validator
 
 import (
+	"fmt"
+	"sort"
+	"strconv"
 	"strings"
 )
 
 func IsValid(acronym string, productName []string) bool {
-	if IsValidCharCount(acronym, productName) && IsValidContainment(acronym, productName) && IsValidCharOrder(acronym, productName) {
-		return true
+	for i, _ := range productName {
+		productName[i] = strings.ToLower(productName[i])
 	}
-	return false
-}
 
-func IsValidCharCount(acronym string, productName []string) bool {
-	acronymSplit := strings.Split(acronym, "")
-	if len(acronymSplit) < len(productName) {
-		return false
-	}
-	return true
-}
+	acronymChars := strings.Split(strings.ToLower(acronym), "")
+	acronymLettersFound := make([]bool, len(acronymChars))
+	productsHavingLetter := make([]bool, len(productName))
 
-func IsValidContainment(acronym string, productName []string) bool {
-	containsAcronym := make([]bool, len(productName))
-	for i, s := range productName {
-		containsAcronym[i] = strings.ContainsAny(strings.ToLower(s), strings.ToLower(acronym))
+	fmt.Println(acronym)
+	fmt.Println(acronymChars)
+
+	hitsOrder := make([][]int, len(productName))
+	for i := range hitsOrder {
+		hitsOrder[i] = make([]int, 0)
 	}
-	for _, b := range containsAcronym {
+
+	productNameLettersMatched := make(map[int][]bool)
+	for i, word := range productName {
+		productNameLettersMatched[i] = make([]bool, len(word))
+	}
+
+	isProductHitInOrder := make([]bool, len(productName))
+
+	fmt.Printf("Testing %v\n", acronymChars)
+	fmt.Printf("Against %v\n", productName)
+	fmt.Println(productNameLettersMatched)
+
+	for wordIndex, productNameWord := range productName {
+		// Checking word GISi, Zombie, Tracker, 3000
+		fmt.Println("....")
+
+		// for letterIndex, acronymLetter := range acronymChars {
+
+		for letterIndex := 0; letterIndex < len(acronymChars); letterIndex++ {
+			acronymLetter := acronymChars[letterIndex]
+			// Checking letters G, I, Z, T, K, 3 in word GISi, Zombie, Tracker, 3000
+			fmt.Println(acronymLetter + " " + strconv.FormatBool(acronymLettersFound[letterIndex]))
+			if strings.Contains(productNameWord, acronymLetter) { // Check if the word contains the acronym letter
+				if !acronymLettersFound[letterIndex] { // Check if the acronym letter has been found
+
+					fmt.Printf("acronym[%v] : %v is contained in productNameWord[%v][%v] : [%v][%v]\n", letterIndex, acronymLetter, wordIndex, strings.Index(productNameWord, acronymLetter), productNameWord, string(productNameWord[strings.Index(productNameWord, acronymLetter)]))
+
+					acronymLettersFound[letterIndex] = true
+					productsHavingLetter[wordIndex] = true
+					hitsOrder[wordIndex] = append(hitsOrder[wordIndex], strings.Index(productNameWord, acronymLetter))
+					productNameLettersMatched[wordIndex][strings.Index(productNameWord, acronymLetter)] = true
+				}
+			}
+
+		}
+	}
+
+	for hitsIndex, hits := range hitsOrder {
+		if sort.IntsAreSorted(hits) {
+			isProductHitInOrder[hitsIndex] = true
+		}
+	}
+
+	fmt.Println(acronymLettersFound)
+	fmt.Println(productsHavingLetter)
+	fmt.Println(isProductHitInOrder)
+	for _, matches := range productNameLettersMatched {
+		fmt.Println(matches)
+	}
+
+	for _, a := range acronymLettersFound {
+		if !a {
+			return false
+		}
+	}
+
+	for _, b := range productsHavingLetter {
 		if !b {
 			return false
 		}
 	}
-	return true
-}
 
-func IsValidCharOrder(acronym string, productName []string) bool {
-	squashedProductName := Squash(productName)
-	acronymSplit := strings.Split(strings.ToLower(acronym), "")
-	indexCounter := -1
-	for _, char := range acronymSplit {
-		if indexCounter <= strings.Index(squashedProductName, char) {
-			indexCounter = strings.Index(squashedProductName, char)
-		} else {
+	for _, c := range isProductHitInOrder {
+		if !c {
 			return false
 		}
 	}
-	return true
-}
 
-func Squash(productName []string) string {
-	return strings.ToLower(strings.Join(productName, ""))
+	return true
 }
