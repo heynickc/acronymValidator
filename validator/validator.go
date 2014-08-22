bcc957013e4ce233aba7f09074b22fc36fed526a
Nick Chamberlain <nick.chamberlain.jr@gmail.com>
2014-08-22 00:08:55 -0400

    New approach just making sure enueration is covered

--- validator/validator.go
+++ validator/validator.go
@@ -8,44 +8,45 @@ import (
 )
 
 func IsValid(acronym string, productName []string) bool {
-	fmt.Println("Testing %v against %v", acronym, productName)
+	fmt.Printf("Testing %v against %v\n", acronym, productName)
 
 	for i, _ := range productName {
 		productName[i] = strings.ToLower(productName[i])
 	}
 
-	squashedProductName := strings.Join(productName, "")
-	squashedProductNameSplit := strings.Split(squashedProductName, "")
-	fmt.Println(squashedProductName)
+	squashedProductName := strings.Join(productName, "|")
+	squashedProductNameChars := strings.Split(squashedProductName, "")
+	acronymChars := strings.Split(strings.ToLower(acronym), "")
+	validAcronymChars := make([]string, 0)
+	productNameContains := make([]bool, len(productName))
 
-	for wordIndex, productNameWord := range productName {
-		// Checking word GISi, Zombie, Tracker, 3000
-		fmt.Println("Testing - " + productNameWord)
-		for letterIndex, acronymLetter := range acronymChars {
-			// Checking letters G, I, Z, T, K, 3 in word GISi, Zombie, Tracker, 3000
+	for _, acronymChar := range acronymChars {
 
-			fmt.Printf("\"%v\"\n", letterIndex)
-
-			if strings.ContainsAny(productNameWord, acronymChars[letterIndex]) { // Check if the word contains any of the acronym letters
-
-				productNameLocation := strings.Index(productNameWord, acronymLetter)
-				productNameSplit := strings.Split(productNameWord, "")
-				productNameSplit = RemoveStringSliceCopy(productNameSplit, productNameLocation, productNameLocation+1)
-				productNameWord = strings.Join(productNameSplit, "")
-
-				letterLocation := strings.Index(squashedProductName, acronymLetter)
-				validAcronymChars = append(validAcronymChars, squashedProductNameSplit[letterLocation])
-				squashedProductNameSplit = RemoveStringSliceCopy(squashedProductNameSplit, letterLocation, letterLocation+1)
-				squashedProductName = strings.Join(squashedProductNameSplit, "")
-
+		for i, product := range productName {
+			if string.Contains(product, acronymChar) {
+				// check for containment
 			}
+		}
+		if strings.Contains(squashedProductName, acronymChar) {
+
+			acronymCharLocation := strings.Index(squashedProductName, acronymChar)
+			// Store valid acronym letter
+			validAcronymChars = append(validAcronymChars, acronymChar)
+			// Remove acronym letter from the product name
+			squashedProductNameChars = squashedProductNameChars[acronymCharLocation:]
+			squashedProductName = strings.Join(squashedProductNameChars, "")
+			// Set original acronym letter to blank
+			// acronymChars[i] = ""
+
+			fmt.Println(squashedProductName)
+			fmt.Println(validAcronymChars)
 		}
 	}
 
-	fmt.Printf("%v == %v?\n", strings.Join(strings.Split(strings.ToLower(acronym), ""), ""), strings.Join(validAcronymChars, ""))
-	if !strings.EqualFold(strings.Join(strings.Split(strings.ToLower(acronym), ""), ""), strings.Join(validAcronymChars, "")) {
-		return false
-	}
+	// fmt.Printf("%v == %v?\n", strings.Join(strings.Split(strings.ToLower(acronym), ""), ""), strings.Join(validAcronymChars, ""))
+	// if !strings.EqualFold(strings.Join(strings.Split(strings.ToLower(acronym), ""), ""), strings.Join(validAcronymChars, "")) {
+	// 	return false
+	// }
 
 	return true
 }
