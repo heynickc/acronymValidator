230c23df89b57a501e2b9bb66db40af8a759415b
Nick Chamberlain <nick.chamberlain.jr@gmail.com>
2014-08-21 22:07:06 -0400

    Refactoring for better duplicate handling

--- validator/validator.go
+++ validator/validator.go
@@ -2,15 +2,21 @@ package validator
 
 import (
 	"fmt"
-	"sort"
-	"strconv"
+	// "sort"
+	// "strconv"
 	"strings"
 )
 
 func IsValid(acronym string, productName []string) bool {
+	fmt.Println("Testing %v against %v", acronym, productName)
+
 	for i, _ := range productName {
 		productName[i] = strings.ToLower(productName[i])
 	}
+
+	squashedProductName := strings.Join(productName, "")
+	squashedProductNameSplit := strings.Split(squashedProductName, "")
+	fmt.Println(squashedProductName)
 
 	acronymChars := strings.Split(strings.ToLower(acronym), "")
 	// Tests if all the letters are used in the productName
@@ -18,92 +24,66 @@ func IsValid(acronym string, productName []string) bool {
 	// Tests if each productName contains a letter
 	productsHavingLetter := make([]bool, len(productName))
 
-	matchIndex := make([]int, len(acronymChars))
-
-	// Tests if the matches are in order...
-	hitsOrder := make([][]int, len(productName))
-	for i := range hitsOrder {
-		hitsOrder[i] = make([]int, 0)
-	}
-
-	// Tests which letters were matched
-	productNameLettersMatched := make(map[int][]bool)
-	for i, word := range productName {
-		productNameLettersMatched[i] = make([]bool, len(word))
-	}
-
-	isProductHitInOrder := make([]bool, len(productName))
-
-	fmt.Printf("Testing %v\n", acronymChars)
-	fmt.Printf("Against %v\n", productName)
-	// fmt.Println(productNameLettersMatched)
-	// fmt.Println(matchIndex)
-
-	flatIndex := 0
+	validAcronymChars := make([]string, 0)
 
 	for wordIndex, productNameWord := range productName {
-
 		// Checking word GISi, Zombie, Tracker, 3000
-		fmt.Println("Test ......")
+		fmt.Println("Testing - " + productNameWord)
 		for letterIndex, acronymLetter := range acronymChars {
-
+			// for i := 0; i < len(acronymChars); i++ {
 			// Checking letters G, I, Z, T, K, 3 in word GISi, Zombie, Tracker, 3000
-			fmt.Println(acronymLetter + " " + strconv.FormatBool(acronymLettersFound[letterIndex]))
+			if strings.ContainsAny(productNameWord, acronymChars[letterIndex]) { // Check if the word contains any of the acronym letters
+				fmt.Printf("acronym[%v] : %v is contained in productNameWord[%v][%v] : [%v][%v]\n", letterIndex, acronymChars[letterIndex], wordIndex, strings.Index(productNameWord, acronymLetter), productNameWord, string(productNameWord[strings.Index(productNameWord, acronymChars[letterIndex])]))
+				fmt.Println(acronymChars[letterIndex])
+				acronymLettersFound[letterIndex] = true
+				productsHavingLetter[wordIndex] = true
 
-			if strings.Contains(productNameWord, acronymLetter) { // Check if the word contains the acronym letter
+				acronymChars[letterIndex] = ""
 
-				if !acronymLettersFound[letterIndex] && !productNameLettersMatched[wordIndex][strings.Index(productNameWord, acronymLetter)] { // Check if the acronym letter has been found and that only the preceding letter has been found
+				productNameLocation := strings.Index(productNameWord, acronymLetter)
+				productNameSplit := strings.Split(productNameWord, "")
+				productNameSplit = RemoveStringSliceCopy(productNameSplit, productNameLocation, productNameLocation+1)
+				productNameWord = strings.Join(productNameSplit, "")
 
-					fmt.Printf("acronym[%v] : %v is contained in productNameWord[%v][%v] : [%v][%v]\n", letterIndex, acronymLetter, wordIndex, strings.Index(productNameWord, acronymLetter), productNameWord, string(productNameWord[strings.Index(productNameWord, acronymLetter)]))
+				letterLocation := strings.Index(squashedProductName, acronymLetter)
 
-					acronymLettersFound[letterIndex] = true
-					productsHavingLetter[wordIndex] = true
-					hitsOrder[wordIndex] = append(hitsOrder[wordIndex], strings.Index(productNameWord, acronymLetter))
-					productNameLettersMatched[wordIndex][strings.Index(productNameWord, acronymLetter)] = true
+				validAcronymChars = append(validAcronymChars, squashedProductNameSplit[letterLocation])
+				squashedProductNameSplit = RemoveStringSliceCopy(squashedProductNameSplit, letterLocation, letterLocation+1)
+				squashedProductName = strings.Join(squashedProductNameSplit, "")
 
-					matchIndex[flatIndex] = letterIndex
-					flatIndex++
-				}
+				// fmt.Println(squashedProductName)
+				// fmt.Println(validAcronymChars)
+				// fmt.Println(acronymChars)
+				// fmt.Println(productNameWord)
 			}
 		}
 	}
 
-	for hitsIndex, hits := range hitsOrder {
-		if sort.IntsAreSorted(hits) {
-			isProductHitInOrder[hitsIndex] = true
-		}
-	}
-
 	fmt.Println(acronymLettersFound)
-	fmt.Println(productsHavingLetter)
-	fmt.Println(isProductHitInOrder)
-	fmt.Println("\nTesting product enumaration...")
-	for _, matches := range productNameLettersMatched {
-		fmt.Println(matches)
-	}
-	// fmt.Println(matchIndex)
-
 	for _, a := range acronymLettersFound {
 		if !a {
 			return false
 		}
 	}
 
+	fmt.Println(productsHavingLetter)
 	for _, b := range productsHavingLetter {
 		if !b {
 			return false
 		}
 	}
 
-	for _, c := range isProductHitInOrder {
-		if !c {
-			return false
-		}
-	}
-
-	if !sort.IntsAreSorted(matchIndex) {
+	fmt.Printf("%v == %v?\n", strings.Join(strings.Split(strings.ToLower(acronym), ""), ""), strings.Join(validAcronymChars, ""))
+	if !strings.EqualFold(strings.Join(strings.Split(strings.ToLower(acronym), ""), ""), strings.Join(validAcronymChars, "")) {
 		return false
 	}
 
 	return true
+}
+
+func RemoveStringSliceCopy(slice []string, start, end int) []string {
+	result := make([]string, len(slice)-(end-start))
+	at := copy(result, slice[:start])
+	copy(result[at:], slice[end:])
+	return result
 }
