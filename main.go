92b5089ef1e18191f2a2ea737b37197cc83039df
Nick Chamberlain <nick.chamberlain.jr@gmail.com>
2014-08-24 17:52:25 -0400

    Local packages

--- main.go
+++ main.go
@@ -1,20 +1,9 @@
 package main
 
-import (
-	"fmt"
-)
-
 // Requirements:
 // Each product name has at least one abbreviation in the acronym
 // Each abbreviation in the acronym has a respective product name
 // The letters in the acronym appear in order for the entire string of product names
 
 func main() {
-	acronymChars := []string{"test", "test"}
-	fmt.Println(pop(acronymChars))
-}
-
-func Pop(strings []string) (string, []string) {
-	popped, strings := strings[len(strings)-1], strings[:len(strings)-1]
-	return popped, strings
 }
