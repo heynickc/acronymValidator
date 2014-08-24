bfe258d84e75d6760824d8f17cf590b841fb3e55
Nick Chamberlain <nick.chamberlain.jr@gmail.com>
2014-08-23 22:48:21 -0400

    Added bucket data structure

--- data_structures/data_structures.go
+++ data_structures/data_structures.go
@@ -2,16 +2,54 @@ package data_structures
 
 import (
 	"fmt"
+	// "errors"
 )
 
 type Bucket struct {
-	name     string
-	items    []string
-	capacity int
+	name              string
+	items             []string
+	capacity          int
+	availableCapacity int
+}
+
+func (b *Bucket) AddItem(item string) bool {
+	if b.availableCapacity > 0 {
+		b.items = InsertStringSlice(b.items, []string{item}, 0)
+		b.availableCapacity--
+		return true
+	} else {
+		return false
+	}
 }
 
 type BucketList struct {
 	buckets []Bucket
+}
+
+func (bl *BucketList) AddItemAtIndex(index int, item string) {
+	ok := bl.buckets[index].AddItem(item)
+	if !ok {
+		fmt.Println("Not ok")
+		fmt.Println(bl.buckets[index].availableCapacity)
+	}
+}
+
+func (bl *BucketList) TryToResizeAtIndex(index int) {
+
+}
+
+func (bl *BucketList) ResetCapacities() {
+	for i := 0; i < len(bl.buckets); i++ {
+		bl.buckets[i].capacity = 0
+		bl.buckets[i].availableCapacity = 0
+	}
+}
+
+func (bl *BucketList) ResetCapacitiesAfterIndex(index int) {
+	for i := index; i < len(bl.buckets); i++ {
+		bl.buckets[i].capacity = 0
+		bl.buckets[i].availableCapacity = 0
+	}
 }
 
 func (bl *BucketList) SetCapacities(count int) {
@@ -21,6 +59,41 @@ func (bl *BucketList) SetCapacities(count int) {
 		}
 		for i := 0; count > 0 && i < len(bl.buckets); i, count = i+1, count-1 {
 			bl.buckets[i].capacity++
+			bl.buckets[i].availableCapacity++
 		}
 	}
+}
+
+func (bl *BucketList) SetCapacitiesAfterIndex(count int, index int) {
+	bl.ResetCapacitiesAfterIndex(index)
+
+	for {
+		if count == 0 {
+			break
+		}
+		for i := index; count > 0 && i < len(bl.buckets); i, count = i+1, count-1 {
+			bl.buckets[i].capacity++
+			bl.buckets[i].availableCapacity++
+		}
+	}
+}
+
+func (bl *BucketList) GetTotalCapacity() int {
+	totalCapacity := 0
+	for _, bucket := range bl.buckets {
+		totalCapacity = totalCapacity + bucket.capacity
+	}
+	return totalCapacity
+}
+
+func (bl *BucketList) GetTotalCapacityAfterIndex(index int) int {
+	totalCapacity := 0
+	for _, bucket := range bl.buckets[index+1:] {
+		totalCapacity = totalCapacity + bucket.capacity
+	}
+	return totalCapacity
+}
+
+func InsertStringSlice(slice, insertion []string, index int) []string {
+	return append(slice[:index], append(insertion, slice[index:]...)...)
 }
--- data_structures/data_structures_test.go
+++ data_structures/data_structures_test.go
@@ -5,14 +5,49 @@ import (
 	"testing"
 )
 
+var bucketList = BucketList{[]Bucket{
+	Bucket{"GISi", make([]string, 0), 0, 0},
+	Bucket{"Zombie", make([]string, 0), 0, 0},
+	Bucket{"Tracker", make([]string, 0), 0, 0},
+	Bucket{"3000", make([]string, 0), 0, 0},
+}}
+
 func TestSetCapacity(t *testing.T) {
-	bucket := Bucket{"TEST", []string{"item", "item2", "item3"}, 0}
-	bucket1 := Bucket{"TEST", []string{"item", "item2", "item3"}, 0}
-	bucket2 := Bucket{"TEST", []string{"item", "item2", "item3"}, 0}
-	bucket3 := Bucket{"TEST", []string{"item", "item2", "item3"}, 0}
-
-	bucketList := BucketList{[]Bucket{bucket, bucket1, bucket2, bucket3}}
-
 	bucketList.SetCapacities(15)
+	fmt.Println(bucketList)
+}
+
+func TestGetTotalCapacity(t *testing.T) {
+	fmt.Println(bucketList.GetTotalCapacity()) // should equal 15
+}
+
+func TestGetTotalCapacityAfterIndex(t *testing.T) {
+	bucketList.SetCapacities(15)
+	fmt.Println(bucketList.GetTotalCapacityAfterIndex(2)) // should equal 3 - 4th item
+}
+
+func TestSetCapacitiesAfterIndex(t *testing.T) {
+	bucketList.SetCapacities(15)
+	bucketList.SetCapacitiesAfterIndex(14, 1)
+	fmt.Println(bucketList)
+}
+
+// func TestAddItemAtIndex(t *testing.T) {
+// 	bucketList.ResetCapacities()
+// 	bucketList.SetCapacities(5)
+// 	itemToAdd := "G"
+// 	bucketList.AddItemAtIndex(0, itemToAdd)
+// 	bucketList.AddItemAtIndex(0, itemToAdd)
+// 	bucketList.AddItemAtIndex(1, itemToAdd)
+// 	fmt.Println(bucketList)
+// }
+
+func TestTryToResizeAtIndex(t *testing.T) {
+	bucketList.ResetCapacities()
+	bucketList.SetCapacities(5)
+	itemToAdd := "G"
+	bucketList.AddItemAtIndex(0, itemToAdd)
+	bucketList.AddItemAtIndex(0, itemToAdd)
+	bucketList.AddItemAtIndex(1, itemToAdd)
 	fmt.Println(bucketList)
 }
